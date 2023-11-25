0f9cb4171ef8ae4079b78ee976b340284dc29b93
Lars Kiesow <lkiesow@uos.de>
2023-11-25 18:27:49 +0100

    Switch to pyproject.toml
    This patch makes the project switch from the legacy specification of
    metadata in the `setup.py` to them being specified in a `pyproject.toml`
    file.

--- setup.py
+++ setup.py
@@ -1,27 +1,3 @@
 from setuptools import setup
-import os
 
-
-def read(filename):
-    path = os.path.abspath(os.path.dirname(__file__))
-    with open(os.path.join(path, filename), encoding='utf-8') as f:
-        return f.read()
-
-
-setup(
-    name='deterrers-cli',
-    version='0.6',
-    description='Command line client for DETERRERS',
-    url='https://github.com/virtUOS/proteuscmd',
-    author='Lars Kiesow',
-    author_email='lkiesow@uos.de',
-    license='MIT',
-    packages=['deterrerscli'],
-    license_files=('LICENSE'),
-    include_package_data=True,
-    install_requires=read('requirements.txt').split(),
-    long_description=read('README.md'),
-    long_description_content_type='text/markdown',
-    entry_points={
-        'console_scripts': ['deterrers-cli = deterrerscli.__main__:cli'],
-    })
+setup()
